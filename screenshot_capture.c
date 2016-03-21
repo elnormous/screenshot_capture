18dcb19fab8890fa51278872d672bee3a253c432
Elviss Strazdins <elviss@elviss.lv>
2016-03-21 15:58:43 +0200

    Code cleanup

--- screenshot_capture.c
+++ screenshot_capture.c
@@ -1,9 +1,5 @@
 //
-//  main.c
 //  ScreenshotCapture
-//
-//  Created by Elviss Strazdins on 14/12/15.
-//  Copyright © 2015 Elviss Strazdins. All rights reserved.
 //
 
 #include <stdio.h>
@@ -14,48 +10,34 @@
 #include <libavfilter/buffersrc.h>
 #include <libavcodec/avcodec.h>
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-
 #define OK 0
 #define ERROR -1
 
-#define BUFFER_SIZE 1024 * 8
-#define MEMORY_STEP 1024
+const size_t BUFFER_SIZE = 1024 * 8;
 
-void log_str(const char * format, ... )
-{
-    va_list vl;
-    va_start(vl, format);
-    
-    vprintf(format, vl);
-    printf("\n");
-    
-    va_end(vl);
-}
-
-
-int get_frame(AVFormatContext *pFormatCtx, AVCodecContext *pCodecCtx, AVFrame *pFrame, int videoStream, int64_t second)
+int get_frame(AVFormatContext *formatCtx, AVCodecContext *codecCtx, AVFrame *frame, int videoStream, int64_t second)
 {
     AVPacket packet;
     int      frameFinished = 0;
     int      rc;
     
-    if ((pFormatCtx->duration > 0) && ((((float_t) pFormatCtx->duration / AV_TIME_BASE) - second)) < 0.1) {
+    if ((formatCtx->duration > 0) && ((((float_t) formatCtx->duration / AV_TIME_BASE) - second)) < 0.1)
+    {
         return ERROR;
     }
     
     rc = ERROR;
     // Find the nearest frame
-    while (!frameFinished && av_read_frame(pFormatCtx, &packet) >= 0) {
+    while (!frameFinished && av_read_frame(formatCtx, &packet) >= 0)
+    {
         // Is this a packet from the video stream?
-        if (packet.stream_index == videoStream) {
+        if (packet.stream_index == videoStream)
+        {
             // Decode video frame
-            avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished, &packet);
+            avcodec_decode_video2(codecCtx, frame, &frameFinished, &packet);
             // Did we get a video frame?
-            if (frameFinished) {
+            if (frameFinished)
+            {
                 rc = OK;
             }
         }
@@ -81,41 +63,42 @@ int display_width(AVCodecContext *pCodecCtx)
 static int get_thumb(const char* filename, const char* out_name)
 {
     int              rc, ret, videoStream;
-    AVFormatContext *pFormatCtx = NULL;
-    AVCodecContext  *pCodecCtx = NULL;
-    AVCodec         *pCodec = NULL;
-    AVFrame         *pFrame = NULL;
-    size_t           uncompressed_size;
+    AVFormatContext *formatCtx = NULL;
+    AVCodecContext  *codecCtx = NULL;
+    AVCodec         *codec = NULL;
+    AVFrame         *frame = NULL;
+    size_t           uncompressedSize;
     unsigned char   *bufferAVIO = NULL;
     int              need_flush = 0;
     char             value[10];
     int              threads = 2;
     int              second = 0;
-    AVCodecContext  *pOCodecCtx = NULL;
-    AVCodec         *pOCodec = NULL;
+    AVCodecContext  *outCodecCtx = NULL;
+    AVCodec         *outCodec = NULL;
     AVPacket        *packet = NULL;
-    AVFrame         *pFrameRGB = NULL;
+    AVFrame         *frameRGB = NULL;
     struct SwsContext *scalerCtx = NULL;
-    AVDictionary    *input_options = NULL;
+    AVDictionary    *inputOptions = NULL;
     char             proto[8];
+    AVDictionary    *dict = NULL;
     
     rc = ERROR;
     
     bufferAVIO = (unsigned char *)malloc(BUFFER_SIZE);
     if (!bufferAVIO)
     {
-        log_str("video thumb extractor module: Couldn't alloc AVIO buffer\n");
+        fprintf(stderr, "video thumb extractor module: Couldn't alloc AVIO buffer\n");
         goto exit;
     }
     
-    pFormatCtx = avformat_alloc_context();
-    if (!pFormatCtx)
+    formatCtx = avformat_alloc_context();
+    if (!formatCtx)
     {
-        log_str("video thumb extractor module: Couldn't alloc AVIO buffer\n");
+        fprintf(stderr, "video thumb extractor module: Couldn't alloc avformat context\n");
         goto exit;
     }
     
-    pFormatCtx->flags |= AVFMT_FLAG_NONBLOCK;
+    formatCtx->flags |= AVFMT_FLAG_NONBLOCK;
     
     av_url_split(proto, sizeof(proto), NULL, 0,
                  NULL, 0, NULL,
@@ -123,71 +106,75 @@ static int get_thumb(const char* filename, const char* out_name)
     
     if (strcmp(proto, "rtmp") == 0)
     {
-        av_dict_set(&input_options, "rtmp_live", "live", 0);
+        av_dict_set(&inputOptions, "rtmp_live", "live", 0);
     }
     
     // Open video file
-    if ((ret = avformat_open_input(&pFormatCtx, filename, NULL, &input_options)) != 0)
+    if ((ret = avformat_open_input(&formatCtx, filename, NULL, &inputOptions)) != 0)
     {
-        log_str("video thumb extractor module: Couldn't open file %s, error: %d\n", filename, ret);
+        fprintf(stderr, "video thumb extractor module: Couldn't open file %s, error: %d\n", filename, ret);
         goto exit;
     }
     
     // Retrieve stream information
-    if (avformat_find_stream_info(pFormatCtx, NULL) < 0)
+    if (avformat_find_stream_info(formatCtx, NULL) < 0)
     {
-        log_str("video thumb extractor module: Couldn't find stream information\n");
+        fprintf(stderr, "video thumb extractor module: Couldn't find stream information\n");
         goto exit;
     }
     
-    if ((pFormatCtx->duration > 0) && ((((float_t) pFormatCtx->duration / AV_TIME_BASE))) < 0.1) {
-        log_str("video thumb extractor module: seconds greater than duration\n");
+    if ((formatCtx->duration > 0) && ((((float_t) formatCtx->duration / AV_TIME_BASE))) < 0.1)
+    {
+        fprintf(stderr, "video thumb extractor module: seconds greater than duration\n");
         rc = ERROR;
         goto exit;
     }
     
     // Find the first video stream
-    videoStream = av_find_best_stream(pFormatCtx, AVMEDIA_TYPE_VIDEO, -1, -1, &pCodec, 0);
-    if (videoStream == -1) {
-        log_str("video thumb extractor module: Didn't find a video stream\n");
+    videoStream = av_find_best_stream(formatCtx, AVMEDIA_TYPE_VIDEO, -1, -1, &codec, 0);
+    if (videoStream == -1)
+    {
+        fprintf(stderr, "video thumb extractor module: Didn't find a video stream\n");
         goto exit;
     }
     
     // Get a pointer to the codec context for the video stream
-    pCodecCtx = pFormatCtx->streams[videoStream]->codec;
+    codecCtx = formatCtx->streams[videoStream]->codec;
     
-    AVDictionary *dict = NULL;
     sprintf(value, "%d", threads);
     av_dict_set(&dict, "threads", value, 0);
     
     // Open codec
-    if ((avcodec_open2(pCodecCtx, pCodec, &dict)) < 0) {
-        log_str("video thumb extractor module: Could not open codec\n");
+    if ((avcodec_open2(codecCtx, codec, &dict)) < 0)
+    {
+        fprintf(stderr, "video thumb extractor module: Could not open codec\n");
         goto exit;
     }
     
     //setup_parameters(cf, ctx, pFormatCtx, pCodecCtx);
     
-    /*if (setup_filters(pFormatCtx, pCodecCtx, videoStream, &filter_graph, &buffersrc_ctx, &buffersink_ctx) < 0) {
+    /*if (setup_filters(formatCtx, codecCtx, videoStream, &filterGraph, &buffersrcCtx, &buffersinkCtx) < 0)
+    {
         goto exit;
     }*/
     
     // Allocate video frame
-    pFrame = av_frame_alloc();
+    frame = av_frame_alloc();
     
-    if (pFrame == NULL) {
-        log_str("video thumb extractor module: Could not alloc frame memory\n");
+    if (frame == NULL) {
+        fprintf(stderr, "video thumb extractor module: Could not alloc frame memory\n");
         goto exit;
     }
     
-    while ((rc = get_frame(pFormatCtx, pCodecCtx, pFrame, videoStream, second)) == 0) {
+    while ((rc = get_frame(formatCtx, codecCtx, frame, videoStream, second)) == 0)
+    {
         
-        if (pFrame->pict_type == 0) { // AV_PICTURE_TYPE_NONE
+        if (frame->pict_type == 0) { // AV_PICTURE_TYPE_NONE
             need_flush = 1;
             break;
         }
         
-        /*if (filter_frame(buffersrc_ctx, buffersink_ctx, pFrame, pFrame) == AVERROR(EAGAIN)) {
+        /*if (filter_frame(buffersrcCtx, buffersinkCtx, frame, frame) == AVERROR(EAGAIN)) {
             need_flush = 1;
             continue;
         }*/
@@ -196,13 +183,15 @@ static int get_thumb(const char* filename, const char* out_name)
         break;
     }
     
-    if (pFrame == NULL || rc != OK) {
-        log_str("Failed to get frame\n");
+    if (frame == NULL || rc != OK) {
+        fprintf(stderr, "Failed to get frame\n");
         goto exit;
     }
     
-    /*if (need_flush) {
-        if (filter_frame(buffersrc_ctx, buffersink_ctx, NULL, pFrame) < 0) {
+    /*if (need_flush)
+    {
+        if (filter_frame(buffersrc_ctx, buffersink_ctx, NULL, pFrame) < 0)
+        {
             goto exit;
         }
         
@@ -220,73 +209,75 @@ static int get_thumb(const char* filename, const char* out_name)
                 (AVPicture*)pFrame, pCodecCtx->pix_fmt, pCodecCtx->width,
                 pCodecCtx->height);*/
     
-    uncompressed_size = pFrame->width * pFrame->height * 3;
+    uncompressedSize = frame->width * frame->height * 3;
     
     //char *newBuffer = malloc(uncompressed_size);
     //av_image_copy_to_buffer(newBuffer, uncompressed_size, pFrame->data, pFrame->linesize, AV_PIX_FMT_RGB24, pFrame->width, pFrame->height);
     
-    log_str("Pixel format: %d, %d", pCodecCtx->pix_fmt, AV_PIX_FMT_YUV420P);
-    log_str("Colorspace: %d", pFrame->colorspace);
+    fprintf(stderr, "Pixel format: %d, %d\n", codecCtx->pix_fmt, AV_PIX_FMT_YUV420P);
+    fprintf(stderr, "Colorspace: %d\n", frame->colorspace);
     
-    scalerCtx = sws_getContext(pCodecCtx->width,
-                               pCodecCtx->height,
-                               pCodecCtx->pix_fmt,
-                               pCodecCtx->width,
-                               pCodecCtx->height,
+    scalerCtx = sws_getContext(codecCtx->width,
+                               codecCtx->height,
+                               codecCtx->pix_fmt,
+                               codecCtx->width,
+                               codecCtx->height,
                                AV_PIX_FMT_RGB24,
                                SWS_BILINEAR, //SWS_BICUBIC
                                NULL, NULL, NULL);
-    if (!scalerCtx) {
+    if (!scalerCtx)
+    {
         printf("sws_getContext() failed\n");
         goto exit;
     }
     
-    pFrameRGB = av_frame_alloc();
+    frameRGB = av_frame_alloc();
     
-    if (pFrameRGB == NULL)
+    if (frameRGB == NULL)
     {
         printf("Failed to alloc frame\n");
         goto exit;
     }
     
-    avpicture_alloc((AVPicture *)pFrameRGB, AV_PIX_FMT_RGB24, pCodecCtx->width, pCodecCtx->height);
+    avpicture_alloc((AVPicture*)frameRGB, AV_PIX_FMT_RGB24, codecCtx->width, codecCtx->height);
     
-    sws_scale(scalerCtx, pFrame->data, pFrame->linesize, 0, pFrame->height, pFrameRGB->data, pFrameRGB->linesize);
+    sws_scale(scalerCtx, frame->data, frame->linesize, 0, frame->height, frameRGB->data, frameRGB->linesize);
     
-    pOCodec = avcodec_find_encoder (AV_CODEC_ID_PNG);
+    outCodec = avcodec_find_encoder (AV_CODEC_ID_PNG);
     
-    if (!pOCodec)
+    if (!outCodec)
     {
-        log_str("Could not find png encode");
+        fprintf(stderr, "Could not find png encode\n");
         goto exit;
     }
     
-    pOCodecCtx = avcodec_alloc_context3(pOCodec);
+    outCodecCtx = avcodec_alloc_context3(outCodec);
     
-    if (!pOCodecCtx) {
-        log_str("Failed to create codec context");
+    if (!outCodecCtx)
+    {
+        fprintf(stderr, "Failed to create codec context\n");
         goto exit;
     }
     
-    pOCodecCtx->bit_rate      = pCodecCtx->bit_rate;
-    pOCodecCtx->width         = pCodecCtx->width;
-    pOCodecCtx->height        = pCodecCtx->height;
-    //pOCodecCtx->pix_fmt       = pCodecCtx->pix_fmt;
-    pOCodecCtx->pix_fmt       = AV_PIX_FMT_RGB24;
-    //pOCodecCtx->codec_id      = AV_CODEC_ID_PNG;
-    //pOCodecCtx->codec_type    = AVMEDIA_TYPE_VIDEO;
-    pOCodecCtx->time_base.num = pCodecCtx->time_base.num;
-    pOCodecCtx->time_base.den = pCodecCtx->time_base.den;
+    outCodecCtx->bit_rate      = codecCtx->bit_rate;
+    outCodecCtx->width         = codecCtx->width;
+    outCodecCtx->height        = codecCtx->height;
+    //outCodecCtx->pix_fmt       = codecCtx->pix_fmt;
+    outCodecCtx->pix_fmt       = AV_PIX_FMT_RGB24;
+    //outCodecCtx->codec_id      = AV_CODEC_ID_PNG;
+    //outCodecCtx->codec_type    = AVMEDIA_TYPE_VIDEO;
+    outCodecCtx->time_base.num = codecCtx->time_base.num;
+    outCodecCtx->time_base.den = codecCtx->time_base.den;
     
     packet = av_packet_alloc();
     
     int got_packet;
     
-    avcodec_encode_video2(pOCodecCtx, packet, pFrameRGB, &got_packet);
+    avcodec_encode_video2(outCodecCtx, packet, frameRGB, &got_packet);
     
     if (!got_packet)
     {
-        log_str("Didn't get packet");
+        fprintf(stderr, "Didn't get packet\n");
         goto exit;
     }
     
@@ -302,23 +293,27 @@ exit:
         av_packet_free(&packet);
     }
     
-    if (pOCodecCtx)
+    if (outCodecCtx)
     {
-        avcodec_free_context(&pOCodecCtx);
+        avcodec_free_context(&outCodecCtx);
     }
     
-    if (pFrameRGB) av_frame_free(&pFrameRGB);
+    if (frameRGB)
+    {
+        avpicture_free((AVPicture*)frameRGB);
+        av_frame_free(&frameRGB);
+    }
     
     // Free the YUV frame
-    if (pFrame) av_frame_free(&pFrame);
+    if (frame) av_frame_free(&frame);
     
     if (scalerCtx) sws_freeContext(scalerCtx);
     
     // Close the codec
-    if (pCodecCtx) avcodec_close(pCodecCtx);
+    if (codecCtx) avcodec_close(codecCtx);
     
     // Close the video file
-    if (pFormatCtx) avformat_close_input(&pFormatCtx);
+    if (formatCtx) avformat_close_input(&formatCtx);
     
     return rc;
 }
@@ -334,13 +329,13 @@ int main(int argc, const char * argv[])
 {
     if (argc < 3)
     {
-        log_str("Too few arguments");
+        fprintf(stderr, "Too few arguments\n");
         return 1;
     }
     
     init();
     
-    log_str("Path: %s", argv[1]);
+    fprintf(stderr, "Path: %s\n", argv[1]);
     
     if (get_thumb(argv[1], argv[2]) != OK)
     {
